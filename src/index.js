ed1a6fdcc7d9a55b97b8ee33daf0d14ead70b2ea
rikimario <mario_martinov@abv.bg>
2023-09-25 23:35:11 +0300

    modified hbs files

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const handlebars = require('express-handlebars');
+const path = require('path');
 
 const app = express();
 const PORT = 5000;
@@ -9,8 +10,10 @@ app.engine('hbs', handlebars.engine({ extname: 'hbs' }));
 app.set('view engine', 'hbs');
 app.set('views', 'src/views');
 
+// Setup static files
+app.use(express.static(path.resolve(__dirname, 'public')));
+
 app.get('/', (req, res) => {
-  // res.send('Hello from express server!')
   res.render('index');
 });
 
