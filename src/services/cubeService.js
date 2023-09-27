dce8ab29f49af49fe19d2fc458e9caf8aa453086
rikimario <mario_martinov@abv.bg>
2023-09-27 21:29:13 +0300

    CUB-P1 implement details page

--- src/services/cubeService.js
+++ src/services/cubeService.js
@@ -1,20 +1,20 @@
 const uniqid = require('uniqid');
 
 const cubes = [
-  // {
-  //   id: '8ewc9vkln0rrvzf',
-  //   name: 'cube1',
-  //   description: 'asd',
-  //   imageUrl: 'https://images.pexels.com/photos/1340185/pexels-photo-1340185.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-  //   difficultyLevel: 1
-  // },
-  // {
-  //   id: '8ewc9vkln0rs8z8',
-  //   name: 'cube2',
-  //   description: 'wqe',
-  //   imageUrl: 'https://images.pexels.com/photos/1340185/pexels-photo-1340185.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-  //   difficultyLevel: 6
-  // }
+  {
+    id: '8ewc9vkln0rrvzf',
+    name: 'cube1',
+    description: 'asd',
+    imageUrl: 'https://images.pexels.com/photos/1340185/pexels-photo-1340185.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    difficultyLevel: 1
+  },
+  {
+    id: '8ewc9vkln0rs8z8',
+    name: 'cube2',
+    description: 'wqe',
+    imageUrl: 'https://images.pexels.com/photos/1340185/pexels-photo-1340185.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    difficultyLevel: 6
+  }
 ];
 
 exports.create = (cubeData) => {
@@ -26,6 +26,27 @@ exports.create = (cubeData) => {
   cubes.push(newCube);
 }
 
-exports.getAll = () => {
-  return [...cubes];
+exports.getAll = (search, from, to) => {
+  let filterCubes = [...cubes];
+
+  if (search) {
+    filterCubes = filterCubes.filter((cube) =>
+      cube.name.toLowerCase().includes(search.toLowerCase()));
+  }
+
+  if (from) {
+    filterCubes = filterCubes.filter((cube) =>
+      cube.difficultyLevel >= Number(from));
+  }
+
+  if (to) {
+    filterCubes = filterCubes.filter((cube) =>
+      cube.difficultyLevel <= Number(to));
+  }
+
+  return filterCubes;
+};
+
+exports.getSingleCube = (id) => {
+  return cubes.find((cube) => cube.id === id);
 };
