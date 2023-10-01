c7025baa288bc755365e587c3b05d480bf5e5294
rikimario <mario_martinov@abv.bg>
2023-10-01 21:43:57 +0300

    added mongoose driver and updated resources

--- src/services/cubeService.js
+++ src/services/cubeService.js
@@ -1,20 +1,20 @@
 const uniqid = require('uniqid');
 
 const cubes = [
-  {
-    id: '8ewc9vkln0rrvzf',
-    name: 'cube1',
-    description: 'asd',
-    imageUrl: 'https://images.pexels.com/photos/1340185/pexels-photo-1340185.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-    difficultyLevel: 1
-  },
-  {
-    id: '8ewc9vkln0rs8z8',
-    name: 'cube2',
-    description: 'wqe',
-    imageUrl: 'https://images.pexels.com/photos/1340185/pexels-photo-1340185.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-    difficultyLevel: 6
-  }
+  // {
+  //   id: '8ewc9vkln0rrvzf',
+  //   name: 'cube1',
+  //   description: 'asd',
+  //   imageUrl: 'https://images.pexels.com/photos/1340185/pexels-photo-1340185.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  //   difficultyLevel: 1
+  // },
+  // {
+  //   id: '8ewc9vkln0rs8z8',
+  //   name: 'cube2',
+  //   description: 'wqe',
+  //   imageUrl: 'https://images.pexels.com/photos/1340185/pexels-photo-1340185.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  //   difficultyLevel: 6
+  // }
 ];
 
 exports.create = (cubeData) => {
