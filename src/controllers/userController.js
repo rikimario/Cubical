b112c95944302f2dea112bb1e9d18af138e3ebe9
rikimario <mario_martinov@abv.bg>
2023-10-07 00:02:45 +0300

    created user controller

---
+++ src/controllers/userController.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+
+router.get('/register', (req, res) => {
+  res.render('user/register');
+});
+
+router.get('/login', (req, res) => {
+  res.render('user/login');
+});
+
+
+module.exports = router;
