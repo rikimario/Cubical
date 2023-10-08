c1b1ef2452f09f5684eb2db6642781b199de78ff
rikimario <mario_martinov@abv.bg>
2023-10-08 23:24:37 +0300

    provide middleware for auth

---
+++ src/middlewares/authMiddleware.js
@@ -0,0 +1,22 @@
+const jwt = require('../lib/jwt');
+const { SECRET } = require('../constants');
+
+exports.auth = async (req, res, next) => {
+  const token = req.cookies['auth'];
+
+  if (token) {
+    try {
+      const decodedToken = await jwt.verify(token, SECRET)
+      req.user = decodedToken;
+      // next();
+    } catch (error) {
+      console.log({ error });
+      res.cookiesClear('auth');
+      res.redirect('/users/login');
+    }
+  } else {
+    next()
+  }
+
+  next();
+};
