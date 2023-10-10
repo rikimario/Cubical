2e5ee4d5c811ab3750263be619e548bba7e8cdb5
rikimario <mario_martinov@abv.bg>
2023-10-10 13:17:25 +0300

    edit and delete get routs

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const cubeService = require('../services/cubeService');
 const accessoryService = require('../services/accessoryService');
+const { difficultyLevelOptionsViewData } = require('../utils/viewData');
 
 router.get('/create', (req, res) => {
   res.render('cube/create');
@@ -62,12 +63,20 @@ router.post('/:cubeId/attach-accessory', async (req, res) => {
   res.redirect(`/cubes/${cubeId}/details`);
 });
 
-router.get('/:cube/edit', (req, res) => {
-  res.render('cube/edit');
+router.get("/:cubeId/edit", async (req, res) => {
+  const { cubeId } = req.params;
+  const cube = await cubeService.getSingleCube(cubeId).lean();
+  const options = difficultyLevelOptionsViewData(cube.difficultyLevel);
+
+  res.render('cube/edit', { cube, options });
 });
 
-router.get('/:cube/delete', (req, res) => {
-  res.render('cube/delete');
+router.get('/:cubeId/delete', async (req, res) => {
+  const { cubeId } = req.params;
+  const cube = await cubeService.getSingleCube(cubeId).lean();
+  const options = difficultyLevelOptionsViewData(cube.difficultyLevel);
+
+  res.render('cube/delete', { cube, options });
 });
 
 module.exports = router;
